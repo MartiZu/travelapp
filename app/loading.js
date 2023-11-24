bea83179351dd38953724e1f7e0e128b7a2e726f
MartiZu <martina.zurli@gmail.com>
2023-11-24 10:41:40 +0100

    loading page

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+      <main className="h-screen flex flex-col justify-center items-center">
+        <h2 className="text-4xl text-text-primary">Loading ...</h2>
+        <p> Just getting it for you...</p>
+      </main>
+    );
+  }
+  
